76ea0c199df10d3782e078b1f20698454ea5ad95
nntnvlogs <lebaobinh20036@gmail.com>
2026-01-06 03:12:38 +0700

    v6

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:5000/api', // Forced Localhost for Debugging
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
+  withCredentials: true,
 });
 
 // Add a request interceptor
